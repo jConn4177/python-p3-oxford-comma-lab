d395ca27656816118c1f7908aafa57601fcb9d7b
Jason <jconn99801@gmail.com>
2023-09-16 11:49:14 -0800

    lab completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
+import ipdb
+
+
 def oxford_comma(items):
-    return None
+    if len(items) < 2:
+        return "".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        return ', '.join(items[:-1]) + ', and ' + items[-1]
